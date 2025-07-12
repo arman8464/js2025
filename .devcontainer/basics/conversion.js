a7d4250344cce9b56c19f37207a6ed781d772821
arman8464 <126165953+arman8464@users.noreply.github.com>
2025-07-12 06:27:50 +0000

    conversion

---
+++ .devcontainer/basics/conversion.js
@@ -0,0 +1,16 @@
+let score = 30
+console.log(typeof score);
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+// 1 >> true
+//0 >> false
+// "" >> false, "arman" >> true
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+let someNumber = 30
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
+
