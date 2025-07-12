51873e2f0bb5dd6479e2eab0b5f3f698e3df6b5e
arman8464 <126165953+arman8464@users.noreply.github.com>
2025-07-12 05:37:37 +0000

    datatypes

---
+++ .devcontainer/basics/datatypes.js
@@ -0,0 +1,13 @@
+let name = "Arman"
+let age = 21
+let isLoggedIn = true
+
+
+// null >> standalone value
+//number >> 2 to power 53
+// bigint >> for large numbers
+// undefined 
+// symbol >> unique
+// object
+console.log(typeof null);
+
--- .devcontainer/basics/variables.js
+++ .devcontainer/basics/variables.js
@@ -1,1 +1,12 @@
-
+const accountId = 234567776
+let accountEmail = "arman@.accountEmail"
+var accountPassword = "1231213"
+accountCity = "Hyd"
+accountEmail = "abd@accountEmail"
+accountPassword = "2222271"
+accountCity = "Blr"
+console.log(accountId);
+console.table([accountEmail,accountPassword,accountCity])
+/*
+do not use var(because of functional scope and block scope) instead use let
+/*
